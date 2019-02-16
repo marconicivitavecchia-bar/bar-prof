05b439c00564ce4eb17477ae7163b5804ef0d3ae
wbigger <wbigger@gmail.com>
2019-02-16 13:02:01 +0100

    Step 1: copy-paste html to app.js, replace static with dynamic content

--- code/app.js
+++ code/app.js
@@ -1,18 +1,25 @@
 var app = {
-    init : function() {
+    init: function () {
         console.log("init inside app!");
         $("title").text("Bar prof");
         $.getJSON("data.json")
-        .done(app.onSuccess)
-        .fail(app.onError);
-        
+            .done(app.onSuccess)
+            .fail(app.onError);
+
     },
-    onSuccess:function(jsonData){
-        // processa i dati qui!!!
+    onSuccess: function (jsonData) {
         console.log(jsonData);
+        jsonData.productList.forEach(element => {
+            let productRow = `<div class="menu-table-row">
+            <div class="menu-table-cell">${element.product}</div>
+            <div class="menu-table-cell">${element.price} euro</div>
+            </div>`;
+            $("#menu-table").append(productRow);
+        });
+
     },
-    
-    onError:function(e){
+
+    onError: function (e) {
         console.log("error!");
         console.log(JSON.stringify(e));
     }
