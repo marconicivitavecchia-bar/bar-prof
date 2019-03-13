5c1124ba77c27f145de429b725b8d3aac376fe1a
wbigger <wbigger@gmail.com>
2019-03-13 13:31:16 +0100

    Add support for mysql and phpmyadmin

--- code/data.php
+++ code/data.php
@@ -9,5 +9,20 @@ $menuObj->productList[1]->product = "Hot Dog";
 $menuObj->productList[1]->price = 2;
 
 $menuJson = json_encode($menuObj);
+
+// $conn = new mysqli("127.0.0.1:3306","user","password","db");
+// if (mysqli_connect_errno()) {
+//     echo '{"error":"error"}';    
+// }
+// Create connection
+$servername = "localhost:3306";
+$username = "root";
+$password = "password";
+$conn = mysqli_connect($servername, $username, $password);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
 echo $menuJson;
 ?>
