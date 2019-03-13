1a769770ccf997147444d26e608469d4d2b62109
wbigger <wbigger@gmail.com>
2019-03-13 15:13:44 +0100

    Fix mysql host according to docker network

--- code/data.php
+++ code/data.php
@@ -2,27 +2,43 @@
 
 // Creo un nuovo oggetto
 $menuObj = new stdClass();
-$menuObj->productList = array(new stdClass(),new stdClass());
+$menuObj->productList = array(new stdClass(), new stdClass());
 $menuObj->productList[0]->product = "Pizzetta";
 $menuObj->productList[0]->price = 1;
 $menuObj->productList[1]->product = "Hot Dog";
 $menuObj->productList[1]->price = 2;
 
 $menuJson = json_encode($menuObj);
+//echo $menuJson;
 
-// $conn = new mysqli("127.0.0.1:3306","user","password","db");
-// if (mysqli_connect_errno()) {
-//     echo '{"error":"error"}';    
-// }
 // Create connection
-$servername = "localhost:3306";
+$servername = "172.26.0.2";
 $username = "root";
 $password = "password";
-$conn = mysqli_connect($servername, $username, $password);
+$dbname = "db";
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
-echo $menuJson;
-?>
+$menuSqlObj = new stdClass();
+$menuSqlObj->productList = array(new stdClass(), new stdClass());
+$sql = "SELECT id,nome,prezzo FROM test";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $menuSqlObj->productList[$row["id"]]->product = $row["nome"];
+        $menuSqlObj->productList[$row["id"]]->price = $row["prezzo"];
+
+        //echo "id: " . $row["id"]. " - Nome: " . $row["nome"]. ", prezzo:" . $row["prezzo"];
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+$menuSqlJson = json_encode($menuSqlObj);
+echo $menuSqlJson;
